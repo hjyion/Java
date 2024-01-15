89c44e9f3c99b3b980ae9532504521b90a398698
hjyion <80759204+hjyion@users.noreply.github.com>
2024-01-15 22:23:50 +0900

    Ch05

---
+++ GoodCalc.java
@@ -0,0 +1,17 @@
+abstract class Calc {
+	public abstract int add(int a, int b);
+}
+
+public class GoodCalc extends Calc {
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GoodCalc c = new GoodCalc();
+		System.out.println(c.add(2, 3));
+
+	}
+
+}
